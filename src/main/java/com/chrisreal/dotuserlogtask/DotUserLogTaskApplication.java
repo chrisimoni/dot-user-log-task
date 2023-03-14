0bb3b09650b87cf9dc3c890bdcf7dc4647cca95e
chris imoni <imonireal@gmail.com>
2023-03-15 00:06:07 +0100

    project setup

---
+++ src/main/java/com/chrisreal/dotuserlogtask/DotUserLogTaskApplication.java
@@ -0,0 +1,13 @@
+package com.chrisreal.dotuserlogtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotUserLogTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotUserLogTaskApplication.class, args);
+	}
+
+}
